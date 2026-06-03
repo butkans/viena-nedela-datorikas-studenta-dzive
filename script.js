690ae0836e83653a2cfb85c621fabf2e70bcfcd8
Ernests Butkans <ernestsbutkans11@gmail.com>
2026-06-03 23:27:33 +0300

    Pabeigta mājaslapa

---
+++ script.js
@@ -0,0 +1,8 @@
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  const isDark = document.body.classList.contains("dark");
+  themeToggle.textContent = isDark ? "Gaišais režīms" : "Tumšais režīms";
+});
